77dff078f7680076dc4cf8e460c2df3846f88334
wyhdag <1550380789@qq.com>
2024-05-10 10:28:10 +0800

    init

--- diy.sh
+++ diy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
@@ -10,13 +10,12 @@
 # See /LICENSE for more information.
 #
 
-
-sed -i 's/Os/O2/g' include/target.mk
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/wyhdag/dns -b v5 package/emortal/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/derisamedia/luci-theme-alpha-alk-net.git package/emortal/luci-theme-alpha-alk
-# sed -i "27a\echo \"DISTRIB_DESCRIPTION=\'$(TZ=UTC-8 date "+%Y-%m-%d") By:WYH\'\" >> /etc/openwrt_release" package/emortal/default-settings/files/99-default-settings
-# sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
+sed -i 's/Os/O2/g' include/target.mk
+sed -i "27a\echo \"DISTRIB_DESCRIPTION=\'$(TZ=UTC-8 date "+%Y-%m-%d") By:WYH\'\" >> /etc/openwrt_release" package/emortal/default-settings/files/99-default-settings
+sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
